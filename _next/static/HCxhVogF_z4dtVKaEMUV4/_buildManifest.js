95839a45204019dacb4c2f4aa89179e72eaca457
yudai1204 <59430508+yudai1204@users.noreply.github.com>
2023-04-23 23:58:51 +0900

    Add files via upload

---
+++ _next/static/HCxhVogF_z4dtVKaEMUV4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/630f77c4e9936f10.css","static/chunks/pages/index-d5eeb2927cb2f868.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
